122960e00b22dbdf6b5424243e1d6531e72d1b9d
Константин Иделевич <kostyaidelevich@gmail.com>
2022-07-17 13:54:09 +0400

    done

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '5576980315:AAEanM14QSf5J43ppTYJn_q8NlJRCCmAzk8'
+
+keys = {
+    'фунт': 'GBP',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'йена': 'JPY',
+    'рубль': 'RUB'
+}
---
+++ extensions.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from config import keys
+
+
+class ConvertionExeption(Exception):
+    pass
+
+
+class Converter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise ConvertionExeption(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionExeption(f'Не удалось обработать валюту {quote}.')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionExeption(f'Не удалось обработать валюту {base}.')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionExeption(f'Не удалось обработать количество {amount}.')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = float(json.loads(r.content)[keys[base]]) * float(amount)
+
+        return total_base
